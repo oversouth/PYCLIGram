73edc752bbb20993adf5f2ad7afcd4155144bb53
Pavel Z <pavel.d.zaikin@gmail.com>
2026-03-26 12:13:22 +0400

    bugfix #1 V1

--- PYCLIGram.py
+++ PYCLIGram.py
@@ -2,7 +2,7 @@ from telethon.sync import TelegramClient
 import os
 api_id = #GET ONE YOURSELF AT https://my.telegram.org
 api_hash = #GET ONE YOURSELF AT https://my.telegram.org
-session_path = os.path.join(os.path.dirname(file), "session")
+session_path = os.path.join(os.path.dirname(__file__), "session")
 with TelegramClient(session_path, api_id, api_hash) as client:
     while True:
         mode = input("\nEnter mode (1 = send, 2 = read, 0 = exit): ")
