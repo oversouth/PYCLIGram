62fa4f6013791661de791689e020e90e6d00b662
Pavel Zaikin <pavel.d.zaikin@gmail.com>
2026-05-02 22:48:13 +0400

    Minor .gitignore changes

--- PYCLIGram.py
+++ PYCLIGram.py
@@ -1,13 +1,17 @@
-from telethon.sync import TelegramClient
-from colorama import Fore, Style, init
 import os
+
+from colorama import Fore, Style, init
+from telethon.sync import TelegramClient
+
 init(autoreset=True)
 api_id = #GET ONE YOURSELF AT https://my.telegram.org
 api_hash = #GET ONE YOURSELF AT https://my.telegram.org
 session_path = os.path.join(os.path.dirname(__file__), "session")
 with TelegramClient(session_path, api_id, api_hash) as client:
     while True:
-        mode = input(f"\n{Fore.MAGENTA}Enter mode (1 = send by username, 2 = send by user ID 3 = read, 0 = exit): {Style.RESET_ALL}")
+        mode = input(
+            f"\n{Fore.MAGENTA}Enter mode (1 = send by username, 2 = send by user ID 3 = read, 0 = exit): {Style.RESET_ALL}"
+        )
         if mode == "1":
             username = input("Enter username without @ : ")
             message = input("Enter message : ")
